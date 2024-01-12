bc8f84fb0c268eede1fb0d489dd26a8c937d2442
DeveloPolaroid <miguelttech.19@gmail.com>
2024-01-11 21:42:11 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,4 @@
-sudo apt install zsh kitty neofetch picom ranger rofi i3status  i3blocks filezilla vlc ffmpeg feh lxappearance nitrogen lsd bat xclip maim pulseaudio-utils openssh-client samba fonts-font-awesome playerctl zsh-autosuggestions zsh-syntax-highlighting git cmus 
-
+sudo apt install zsh kitty neofetch picom ranger rofi i3status  i3blocks filezilla vlc ffmpeg feh lxappearance nitrogen lsd bat xclip maim pulseaudio-utils openssh-client samba fonts-font-awesome playerctl zsh-autosuggestions zsh-syntax-highlighting git cmus bashtop
 # Install oh_my_zsh, p10k and fonts
 wget  https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip &&
 mkdir ~/.local/share/fonts && 
@@ -19,5 +18,11 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git &&
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Rofi
+mkdir -p ~/.local/share/rofi/themes
+git clone https://github.com/lr-tech/rofi-themes-collection.git &&
+mv rofi-themes-collection/themes/* ~/.local/share/rofi/themes/
 
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
