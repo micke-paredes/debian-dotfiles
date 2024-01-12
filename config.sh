7c781cf368bbbc8caf94f20edfe9d096b0f1e515
DeveloPolaroid <miguelttech.19@gmail.com>
2024-01-11 20:53:09 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,16 @@
 sudo apt install zsh kitty neofetch picom ranger rofi i3status  i3blocks filezilla vlc ffmpeg feh lxappearance nitrogen lsd bat xclip maim pulseaudio-utils openssh-client samba fonts-font-awesome playerctl zsh-autosuggestions zsh-syntax-highlighting git cmus 
 
-# Install oh_my_zsh
+# Install oh_my_zsh, p10k and fonts
+wget  https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip &&
+mkdir ~/.local/share/fonts && 
+unzip Hack.zip -d ~/.local/share/fonts && 
+rm  *.zip
+sudo fc-cache -v
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
 
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
