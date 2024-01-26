f1e9e2b746615d7a401caeeedc229bef5727f6cc
DeveloPolaroid <miguelttech.19@gmail.com>
2024-01-26 12:30:10 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y wget zsh kitty neofetch picom ranger rofi i3status  i3blocks filezilla vlc ffmpeg feh lxappearance nitrogen lsd bat xclip maim pulseaudio-utils openssh-client samba fonts-font-awesome playerctl zsh-autosuggestions zsh-syntax-highlighting git cmus nodejs npm apache2 default-mysql-client default-mysql-server default-mysql-server-core mysql-common ufw libhidapi-libusb0 python3-pip thunderbird pavucontrol
+sudo apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util0-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
 
 # Rofi
 mkdir -p ~/.local/share/rofi/themes
@@ -28,6 +29,26 @@ git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git &&
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+# Make config directories
+mkdir -p ~/.config/i3 ~/.config/rofi ~/.config/picom
+
+# replace zsrch, i3, picom files
+rm ~/.zshrc
+rm ~/.config/i3/config
+mv zshrc-template .zshrc
+mv i3-config ~/.config/i3/config
+
+# Alternating layout
+git clone https://github.com/olemartinorg/i3-alternating-layout
+cd i3-alternating-layout
+sudo mv alternating_layouts.py /bin/alternating_layouts.py
+
+# i3 lock color
+git clone https://github.com/Raymo111/i3lock-color.git
+cd i3lock-color
+./build.sh
+./install-i3lock-color.sh
 
 # Config samba
 
